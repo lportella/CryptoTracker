4f7ff771d594d79400a79f3d1368b14f2441c885
Lucas Portella <lucas.portellasantos@gmail.com>
2025-01-27 18:30:57 -0300

    add PriceChange component

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
@@ -2,19 +2,27 @@ package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.crypto.presentation.models.CoinUI
 import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUI
@@ -26,6 +34,11 @@ fun CoinListItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val contentColor = if (isSystemInDarkTheme()) {
+        Color.White
+    } else {
+        Color.Black
+    }
     Row(
         modifier = modifier
             .clickable(onClick = onClick)
@@ -39,6 +52,36 @@ fun CoinListItem(
             tint = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(85.dp)
         )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = coinUI.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = contentColor
+            )
+            Text(
+                text = coinUI.name,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Light,
+                color = contentColor
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "$ ${coinUI.priceUsd.formatted}",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = contentColor
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            PriceChange(
+                change = coinUI.changePercentage24Hr
+            )
+        }
     }
 }
 
@@ -59,7 +102,7 @@ private fun CoinListItemPreview() {
 internal val previewCoin = Coin(
     id = "bitcoin",
     rank = 1,
-    name = " Bitcoin",
+    name = "Bitcoin",
     symbol = "BTC",
     marketCapUsd = 12265423458896.74,
     priceUsd = 62888.15,
