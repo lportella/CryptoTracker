d71b06d3f45f613df3769c1516ff7af3976e6a09
Lucas Portella <lucas.portellasantos@gmail.com>
2025-02-09 23:45:17 -0300

    add error handling

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -6,9 +6,11 @@ import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUI
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -33,6 +35,9 @@ class CoinListViewModel(
         }
     }
 
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
+
     private fun loadCoins() {
         viewModelScope.launch {
             _state.update { it.copy(
@@ -51,6 +56,7 @@ class CoinListViewModel(
                     _state.update { it.copy(
                         isLoading = false
                     )}
+                    _events.send(CoinListEvent.Error(error))
                 }
         }
     }
